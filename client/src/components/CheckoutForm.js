e0f42ae262fb12e12421e9a8672f5e810e54a2e0
Your Name <you@example.com>
2021-01-15 12:59:25 -0600

    5 - Testing Stuff

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -37,6 +37,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
+            id="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
@@ -45,6 +46,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
+            id="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
@@ -53,6 +55,7 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
+            id="address"
             name="address"
             value={values.address}
             onChange={handleChanges}
@@ -60,15 +63,15 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input id="city" name="city" value={values.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input id="state" name="state" value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input id="zip" name="zip" value={values.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,50 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+    render(<CheckoutForm/>);
+
+});
+
+test("form shows success message on submit with form details", async () => {
+    //Act:
+    //  1. Get our input fields.
+    const fNameInput = screen.getByLabelText(/first name/i);
+    const lNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    //  2. Type values into our input fields.
+    userEvent.type(fNameInput, "Bob");
+    userEvent.type(lNameInput, "Bobblin");
+    userEvent.type(addressInput, "4321 South Bobbles St");
+    userEvent.type(cityInput, "Bobville");
+    userEvent.type(stateInput, "Bexus");
+    userEvent.type(zipInput, "60602");
+
+    //  3. Push the submit button.
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    
+    //Assert:
+    // Info is on the screen.
+    const newfName = await screen.findByDisplayValue("Bob");
+    const newlName = await screen.findByDisplayValue("Bobblin"); 
+    const newAddress = await screen.findByDisplayValue("4321 South Bobbles St");  
+    const newCity = await screen.findByDisplayValue("Bobville");
+    const newState = await screen.findByDisplayValue("Bexus");  
+    const newZip = await screen.findByDisplayValue("60602");
+    expect(newfName).toBeInTheDocument();
+    expect(newlName).toBeInTheDocument();
+    expect(newAddress).toBeInTheDocument();
+    expect(newCity).toBeInTheDocument(); 
+    expect(newState).toBeInTheDocument();
+    expect(newZip).toBeInTheDocument(); 
+});
